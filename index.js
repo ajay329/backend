e4fef44c02a71d0e280e382442ce02398dba3e85
sonukalakar <ajaypandat329@gmail.com>
2023-01-19 15:29:25 +0530

    backend

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const signupRoute = require('./routes/signup.routes')
+const loginRoute = require('./routes/login.route')
+const connect = require('./config/db')
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(cors())  // run frnt bckd at same local port
+app.use("/signup",signupRoute)
+app.use("/login",loginRoute)
+
+
+app.get('/', (req, res) => res.send('hello'))
+
+app.listen(8080, async () => {
+    await connect();
+    console.log("mongodb connected at 8080");
+    try {
+    } catch (e) {
+      console.log(e);
+    }
+  });
